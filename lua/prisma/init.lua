bab77a8ba7a095bd239f6b554b512d7f39b53f20
Emmanuel Asiimwe <90935867+asiimwemmanuel@users.noreply.github.com>
2025-11-04 10:45:03 +0300

    migrating from nvim-cmp to blink.cmp

--- lua/prisma/init.lua
+++ lua/prisma/init.lua
@@ -1,29 +1,29 @@
 local M = {}
 
 function M.setup(opts)
-    local conf = vim.tbl_deep_extend("force", {
-        auto_format = true,
-        lsp = {
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-            on_attach = nil,
-            settings = {}
-        }
-    }, opts or {})
+	local conf = vim.tbl_deep_extend("force", {
+		auto_format = true,
+		lsp = {
+			capabilities = require("blink.cmp").get_lsp_capabilities(),
+			on_attach = nil,
+			settings = {},
+		},
+	}, opts or {})
 
-    local mason_registry = require("mason-registry")
-    if not mason_registry.is_installed("prisma-language-server") then
-        vim.cmd.MasonInstall("prisma-language-server")
-    end
+	local mason_registry = require("mason-registry")
+	if not mason_registry.is_installed("prisma-language-server") then
+		vim.cmd.MasonInstall("prisma-language-server")
+	end
 
-    local has_parser = pcall(function()
-        return require("nvim-treesitter.parsers").get_parser_configs().prisma
-    end)
+	local has_parser = pcall(function()
+		return require("nvim-treesitter.parsers").get_parser_configs().prisma
+	end)
 
-    if not has_parser then
-        vim.cmd.TSInstall("prisma")
-    end
+	if not has_parser then
+		vim.cmd.TSInstall("prisma")
+	end
 
-    require('prisma.lsp').setup(conf)
+	require("prisma.lsp").setup(conf)
 end
 
 return M
